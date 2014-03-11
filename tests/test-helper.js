41e3d8b7e8e24d0d77172bcc3bf77feb3ccce238
Ohno Shinichi <ohno@cookpad.com>
2014-03-11 17:46:01 +0900

    Clenup sample code and set up jasmine2

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -5,26 +5,4 @@ require('ember-qunit').setResolver(resolver);
 
 
 window.startApp          = require('appkit/tests/helpers/start-app')['default'];
-window.isolatedContainer = require('ember-qunit/isolated-container')['default'];
-
-function exists(selector, context) {
-  return !!find(selector, context).length;
-}
-
-function getAssertionMessage(actual, expected, message) {
-  return message || QUnit.jsDump.parse(expected) + " expected but was " + QUnit.jsDump.parse(actual);
-}
-
-function equal(actual, expected, message) {
-  message = getAssertionMessage(actual, expected, message);
-  QUnit.equal.call(this, actual, expected, message);
-}
-
-function strictEqual(actual, expected, message) {
-  message = getAssertionMessage(actual, expected, message);
-  QUnit.strictEqual.call(this, actual, expected, message);
-}
-
-window.exists = exists;
-window.equal = equal;
-window.strictEqual = strictEqual;
+window.isolatedContainer = require('appkit/tests/helpers/isolated-container')['default'];
--- tests/test-loader.js
+++ tests/test-loader.js
@@ -1,6 +1,7 @@
 // TODO: load based on params
-Ember.keys(requirejs.entries).forEach(function(entry) {
-  if ((/\-test/).test(entry)) {
-    require(entry, null, null, true);
-  }
+
+Ember.keys(requirejs._eak_seen).filter(function(key) {
+  return (/Spec/).test(key);
+}).forEach(function(moduleName) {
+  require(moduleName, null, null, true);
 });
