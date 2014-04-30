e52e34331138c2c6b005e4a3d16d67f9320e7197
Shin'ichi Ohno <shin1ohno@me.com>
2014-04-30 10:46:37 +0900

    Fix test helper

--- spec/test-helper.js
+++ spec/test-helper.js
@@ -5,4 +5,3 @@ Ember.testing = true;
 var resolver = require('appkit/spec/helpers/resolver')['default'];
 
 window.startApp          = require('appkit/spec/helpers/start-app')['default'];
-window.isolatedContainer = require('appkit/spec/helpers/isolated-container')['default'];
